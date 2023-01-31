142f57c89cd27c72f646b3dd6d71cb6cdf4719a7
Oleynik Andrey <andry44666@gmail.com>
2023-02-01 02:45:37 +0300

    Complited zadacha_1

--- Zadacha_1_seminar_2/Program.cs
+++ Zadacha_1_seminar_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Enter number");
+string? strNumber = System.Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+if (number > 100 && number < 1000)
+{
+number = number % 100;
+number = number / 10;
+System.Console.WriteLine(number);
+}
+else
+{
+    System.Console.WriteLine("number not three-digit");
+}
