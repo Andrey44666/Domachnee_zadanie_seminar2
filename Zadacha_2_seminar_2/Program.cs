ae79c03f7b68ac0fc18617da3223528db993464e
Oleynik Andrey <andry44666@gmail.com>
2023-02-02 01:11:00 +0300

    Complited Zadacha_2_seminar_2

---
+++ Zadacha_2_seminar_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Enter number");
+int number = int.Parse(System.Console.ReadLine());
+int numberB = 1000;
+while (numberB <number)
+{
+    numberB = number * 10;
+}
+if  (number < 100)
+{
+    System.Console.WriteLine("Third digit is not");
+}
+else
+{
+    number = number % (numberB / 100);
+    System.Console.WriteLine(number);
+}       
